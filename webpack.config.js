c0a8d736c95de78b3a60afe269b9008ccae65037
Ramazan <108758151+inuazh@users.noreply.github.com>
2024-10-10 23:42:34 +0300

    range_start

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const config = {
 	entry: {
 		index: './src/js/index.js',  // Точка входа для index.js
 		catalog: './src/js/catalog.js',  // Точка входа для catalog.js
-		card: './src/js/card.js'  // Точка входа для catalog.js
+		card: './src/js/card.js'  // Точка входа для card.js
 	},
 	output: {
-		filename: '[name].bundle.js',  // [name] будет заменён на 'index' и 'catalog'
+		filename: '[name].bundle.js',  // [name] будет заменён на 'index' и 'catalog' и 'card'
 	},
 	module: {
 		rules: [
